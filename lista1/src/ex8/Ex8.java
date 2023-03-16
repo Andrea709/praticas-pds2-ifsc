16022baa136ebda6a9c523894eb03daec2da5675
nog <andrea.n1@aluno.ifsc.edu.br>
2023-03-16 09:13:39 -0300

    Banco de dados conectado ebaaaaa
    LG lindo salvou minha pele

--- lista1/src/ex8/Ex8.java
+++ lista1/src/ex8/Ex8.java
@@ -8,13 +8,21 @@ import java.sql.Statement;
 public class Ex8 {
 	public static void main(String[] args) {
 		try {
-		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/"+"?serverTimezone=UTC","root","aluno");
+		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/","root","aluno");
 		
 		Statement stm = conexao.createStatement();
-		String WSQL = "create database Nog";
-		stm.executeQuery(WSQL);
-		WSQL = "create table Pessoa";
-		stm.executeQuery(WSQL);
+		String WSQL;
+//		WSQL = "create database Nog01";
+//		stm.executeUpdate(WSQL);
+		
+		WSQL = "use Nog01";
+		stm.executeUpdate(WSQL);
+		
+		WSQL = "create table Pessoa ("
+				+ "	cpf int not null primary key,"
+				+ "    nome varchar(50)"
+				+ ")";
+		stm.executeUpdate(WSQL);
 		
 		
 		
